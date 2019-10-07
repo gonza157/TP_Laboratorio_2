1618a000a86c2cc4aeb0436f365974fb5b8b61e5
sashalospalluto <36174607+gonza157@users.noreply.github.com>
2019-10-06 22:03:52 -0300

    TP2_Laboratorio2

---
+++ TP-02/Entidades/Leche.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace Entidades_2018
+{
+    public class Leche : Producto
+    {
+        public enum ETipo { Entera, Descremada }
+        ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="codigo"></param>
+        /// <param name="color"></param>
+        public Leche(Producto.EMarca marca, string codigo, ConsoleColor color)
+            : base(codigo, marca, color)
+        {
+            tipo = ETipo.Entera;
+        }
+
+        public Leche(Producto.EMarca marca, string codigo, ConsoleColor color, ETipo tipo) : this(marca,codigo,color)
+        {
+            this.tipo=tipo;
+        }
+
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+        /// <summary>
+        /// Sobrecarga del metodo mostrar de producto
+        /// </summary>
+        /// <returns> string con todos los datos del producto</returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
